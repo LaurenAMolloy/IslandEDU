6d0d3b508dfdb1f4e251ef4d8fadf1c815bda349
lauren_m <laurenamolloy@gmail.com>
2026-02-11 12:58:24 +0200

    Adding boilerplate for views

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path')
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const methodOverride = require('method-override')
 const School = require('./models/school')
 
 //Connect to Mongo
@@ -18,13 +20,15 @@ db.once("open", () => {
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 //absolute path /Users/you/projects/island-edu/views
 //builds safely
 //bullet proof path
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.get("/", (res, req) => {
     res.render('home')
@@ -57,7 +61,24 @@ app.get('/schools/:id', async(req, res) => {
     res.render('schools/show', { school })  
 })
 
+//Update School by ID
+app.get('/schools/:id/edit', async(req, res) => {
+    const { id } = req.params
+    const school = await School.findById(id)
+    res.render('schools/edit', { school })
+});
 
+app.put('/schools/:id', async(req, res) => {
+   const { id } = req.params;
+   const school = await School.findByIdAndUpdate(id, { ...req.body.school })
+   res.redirect(`/schools/${school._id}`)
+})
+
+app.delete('/schools/:id', async(req, res) => {
+    const { id } = req.params;
+    const school = await School.findByIdAndDelete(id);
+    res.redirect('/schools')
+})
 
 app.listen(8000, ()=> {
     console.log("Listening on Port 8000")
