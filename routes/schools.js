ad611cab13b3a5d7647ed6897ca794a0c341f008
lauren_m <laurenamolloy@gmail.com>
2026-03-18 11:22:16 +0200

    Refactoring routes for readability

--- routes/schools.js
+++ routes/schools.js
@@ -6,28 +6,19 @@ const { isLoggedIn, isAuthor, validateSchool } = require('../middleware');
 
 const School = require('../models/school');
 
-//GET all schools
-router.get("/", catchAsync(schools.index));
+//Fancy way to route
+router.route('/')
+    .get(catchAsync(schools.index))
+    .post(isLoggedIn, validateSchool, catchAsync(schools.createSchool))
 
-//Create is two routes!
-//Why?
-//GET the form
-//console.log(typeof isLoggedIn)
 router.get('/new', isLoggedIn, schools.newForm);
 
-//POST the data
-//Add the catch Async function
-//User now has to be loggedin to access the create school
-router.post('/', isLoggedIn, validateSchool, catchAsync(schools.createSchool))
+router.route('/:id')
+    .get(catchAsync(schools.showSchool))
+    .put(isLoggedIn, isAuthor, validateSchool, catchAsync(schools.updateSchool))
+    .delete(catchAsync(schools.deleteSchool));
 
-//GET school by id
-router.get('/:id', catchAsync(schools.showSchool))
 
-//Update School by ID
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(schools.editSchoolForm));
-
-router.put('/:id', isLoggedIn, isAuthor, validateSchool, catchAsync(schools.updateSchool))
-
-router.delete('/:id', catchAsync(schools.deleteSchool));
 
 module.exports = router
--- routes/users.js
+++ routes/users.js
@@ -7,13 +7,13 @@ const { storeReturnTo } = require('../middleware');
 const users = require('../controllers/users');
 
 
-router.get('/register', users.registerForm)
+router.route('/register')
+.get(users.registerForm)
+.post(catchAsync( users.register));
 
-router.post('/register', catchAsync( users.register));
-
-router.get('/login', users.loginForm);
-
-router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
+router.route('/login')
+    .get(users.loginForm)
+    .post(storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
 
 router.get('/logout', users.logout);
 
