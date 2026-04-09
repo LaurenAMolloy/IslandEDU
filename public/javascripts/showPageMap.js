571ce3d9014f60cd2d8075696f8b556fe50fe638
lauren_m <laurenamolloy@gmail.com>
2026-04-09 18:21:08 +0100

    Adding map to show page

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,19 +1,17 @@
-maptilersdk.config.apiKey = maptilerApiKey;
-//console.log(school);
+ 
+        mapboxgl.accessToken = mapToken;
+        const map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/standard', // Use the standard style for the map
+            //projection: 'globe', // display the map as a globe
+            zoom: 8, // initial zoom level, 0 is the world view, higher values zoom in
+            center: [33.36, 35.17] // center the map on this longitude and latitude
+        });
 
-const map = new maptilersdk.Map({
-    container: 'map',
-    style: maptilersdk.MapStyle.BRIGHT,
-    center: school.geometry.coordinates, // starting position [lng, lat]
-    zoom: 10 // starting zoom
-});
+        map.addControl(new mapboxgl.NavigationControl());
+        map.scrollZoom.disable();
 
-new maptilersdk.Marker()
-    .setLngLat(school.geometry.coordinates)
-    .setPopup(
-        new maptilersdk.Popup({ offset: 25 })
-            .setHTML(
-                `<h3>${school.title}</h3><p>${school.location}</p>`
-            )
-    )
-    .addTo(map)
+        map.on('style.load', () => {
+            map.setFog({}); // Set the default atmosphere style
+        });
+   
