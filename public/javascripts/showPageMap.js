a6b5ad8bba5cc49a96684dbf021aab4644d566e2
lauren_m <laurenamolloy@gmail.com>
2026-04-10 17:11:28 +0100

    Adding marker to map

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -4,9 +4,13 @@
             container: 'map',
             style: 'mapbox://styles/mapbox/standard', // Use the standard style for the map
             //projection: 'globe', // display the map as a globe
-            zoom: 8, // initial zoom level, 0 is the world view, higher values zoom in
-            center: [33.36, 35.17] // center the map on this longitude and latitude
+            zoom: 10, // initial zoom level, 0 is the world view, higher values zoom in
+            center: school.geometry.coordinates, // center the map on this longitude and latitude
         });
+
+        const marker = new mapboxgl.Marker()
+        .setLngLat(school.geometry.coordinates,)
+        .addTo(map)
 
         map.addControl(new mapboxgl.NavigationControl());
         map.scrollZoom.disable();
